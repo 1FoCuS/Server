f01fb53d4709aad80437ae217d62fadab605ea47
Constantine Morozov <34001730+1FoCuS@users.noreply.github.com>
2020-02-13 21:24:49 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -5,23 +5,49 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char** argv)
 {
     int status;
-    struct addrinfo hints;
-    struct addrinfo* servinfo;
-
+    struct addrinfo hints, *p, *res;
+    char ipstr[INET6_ADDRSTRLEN];
+    //struct addrinfo* servinfo;
+    if (argc!=2)
+    {
+        fprintf(stderr, "usage: show ip hostname\n");
+        return 1;
+    }
     memset(&hints, 0, sizeof(hints));
+
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+    //hints.ai_flags = AI_PASSIVE;
 
-    if ((status=getaddrinfo(NULL, "3490", &hints, &servinfo))!=0)
+    if ((status=getaddrinfo(argv[1], NULL, &hints, &res))!=0)
     {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-        exit(1);
+        return 2;
+    }
+    printf("ip addresses for %s\n\n", argv[1]);
+    for(p=res; p!=NULL; p=p->ai_next)
+    {
+        void *addr;
+        char *ipver;
+        if (p->ai_family == AF_INET)
+        {
+            struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else
+        {
+            struct sockaddr_in6* ipv4 = (struct sockaddr_in6*)p->ai_addr;
+            addr = &(ipv4->sin6_addr);
+            ipver = "IPv6";
+        }
+        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+        printf("%s %s\n", ipver, ipstr);
     }
 
-    freeaddrinfo(servinfo);
+
+    freeaddrinfo(res);
     return 0;
 }
