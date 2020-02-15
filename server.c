8b41f5b498ba3b6cf51e142aa06a537be4fb4167
Constantine Morozov <34001730+1FoCuS@users.noreply.github.com>
2020-02-15 23:33:28 +0300

    Update server.c

--- server.c
+++ server.c
@@ -9,8 +9,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define PORT ("3490")
+#define PORT ("3491")
 #define BACKLOG (10)
+#define MAXDATASIZE 256
 #define UNUSADE(x)  ((x)==(x))
 
 void sigchld_handler(int s) {
@@ -23,42 +24,52 @@ void *get_in_addr(struct sockaddr* sa) {
     }
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
-int main()
-{
-    int sockfd, newfd;
-    struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr;
-    socklen_t sin_size;
-    struct sigaction sa;
-    int yes=1;
-    char s[INET6_ADDRSTRLEN];
-    int rv;
-    // ************************************************************************************
+void print_hostname(void) {
+    char host_name[MAXDATASIZE];
+    int er;
+    if ((er=gethostname(host_name, MAXDATASIZE))==-1) {
+        perror("error name");
+        exit(1);
+    }
+    printf("Host-name: %s\n", host_name);
+}
+
+struct addrinfo* init_server(void) {
+    struct addrinfo hints, *server_info;
+
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;            // my IP
 
-    if ((rv=getaddrinfo(NULL, PORT, &hints, &servinfo))==-1) {
+    int rv;
+    if ((rv=getaddrinfo(NULL, PORT, &hints, &server_info))==-1) {
         fprintf(stderr, "bad getaddrinfo: %s\n\n", gai_strerror(rv));
-        return 1;
+        exit(1);
     }
 
-    for(p = servinfo; p!=NULL; p=p->ai_next) {
+    return server_info;
+}
 
-        if((sockfd=socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+int init_socket(struct addrinfo *server_info) {
+    int socketfd;
+    int yes=1;
+    struct addrinfo *p;
+
+    for(p = server_info; p!=NULL; p=p->ai_next) {
+        if((socketfd=socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror(("socket::server"));
-            continue;
+            return -1;
         }
         // read
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == 1) {
+        if (setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == 1) {
             perror("setsockopt");
-            return 2;
+            exit(1);
         }
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == 1) {
-            close(sockfd);
+        if (bind(socketfd, p->ai_addr, p->ai_addrlen) == 1) {
+            close(socketfd);
             perror("bind");
-            continue;
+            return -1;
         }
         break;
     }
@@ -67,11 +78,30 @@ int main()
         fprintf(stderr, "server failed to bind\n");
         return 3;
     }
-    freeaddrinfo(servinfo);
-    // ====================================================================================
-    // ************************************************************************************
+    freeaddrinfo(server_info);
 
-    if (listen(sockfd, BACKLOG)==-1) {
+    return socketfd;
+}
+
+//*******************************************************************************************
+
+int main(int argc, char *argv[]) {
+    UNUSADE(argc);
+    UNUSADE(argv);
+
+    int newfd;
+    struct sockaddr_storage their_addr;
+
+    socklen_t sin_size;
+    struct sigaction sa;
+    char s[INET6_ADDRSTRLEN];
+
+    print_hostname();
+    struct addrinfo *server_info = init_server();
+    int socketfd = init_socket(server_info);
+
+    // ====================================================================================
+    if (listen(socketfd, BACKLOG)==-1) {
         perror("listen");
         return 4;
     }
@@ -82,14 +112,13 @@ int main()
         perror("sigaction");
         return 5;
     }
-    // ====================================================================================
     // ************************************************************************************
 
     printf("Server wait to connections...\n");
 
     while (1) {
         sin_size = sizeof(their_addr);
-        newfd = accept(sockfd, (struct sockaddr*)&their_addr, &sin_size);
+        newfd = accept(socketfd, (struct sockaddr*)&their_addr, &sin_size);
         if (newfd==-1){
             perror("accept");
             continue;
@@ -97,15 +126,21 @@ int main()
         inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr*)&their_addr), s, sizeof(s));
         printf("Server got a connection from %s\n", s);
 
-
         if (!fork()) {
-            close(sockfd);
+            printf("work fork\n");
+            close(socketfd);
             if (send(newfd, "my first socket-server", 22, 0) == -1) {
                 perror("send");
             }
             close(newfd);
             exit(0);
         }
+
+        /*
+        if (send(newfd, "my first socket-server", 22, 0) == -1) {
+            perror("send");
+        }
+        */
         close(newfd);
     }
     return 0;
