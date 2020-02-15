ef9da871218670fa66639312e1f214e5d1f1c26f
Constantine Morozov <34001730+1FoCuS@users.noreply.github.com>
2020-02-15 13:54:06 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,56 +2,111 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <signal.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char** argv)
+#define PORT ("3490")
+#define BACKLOG (10)
+#define UNUSADE(x)  ((x)==(x))
+
+void sigchld_handler(int s) {
+    UNUSADE(s);
+    while(waitpid(-1, NULL, WNOHANG)>0);
+}
+void *get_in_addr(struct sockaddr* sa) {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+int main()
 {
-    int status;
-    struct addrinfo hints, *p, *res;
-    char ipstr[INET6_ADDRSTRLEN];
-
-    /*if (argc!=2)
-    {
-        fprintf(stderr, "usage: show ip hostname\n");
-        return 1;
-    }*/
-    const char * site = "www.vk.com";
-
+    int sockfd, newfd;
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr;
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+    // ************************************************************************************
     memset(&hints, 0, sizeof(hints));
-
-    hints.ai_family = AF_UNSPEC;            // IPv4 and IPv6
+    hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;            // my IP
 
-    if ((status=getaddrinfo(site, NULL, &hints, &res))!=0)
-    {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-        return 2;
+    if ((rv=getaddrinfo(NULL, PORT, &hints, &servinfo))==-1) {
+        fprintf(stderr, "bad getaddrinfo: %s\n\n", gai_strerror(rv));
+        return 1;
     }
-    printf("ip addresses for %s\n\n", site);
-    for(p=res; p!=NULL; p=p->ai_next)
-    {
-        void *addr;
-        int port;
-        char *ipver;
-        if (p->ai_family == AF_INET)
-        {
-            struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
-            addr = &(ipv4->sin_addr);
-            port = (ipv4->sin_port);
-            ipver = "IPv4";
-        } else
-        {
-            struct sockaddr_in6* ipv6 = (struct sockaddr_in6*)p->ai_addr;
-            addr = &(ipv6->sin6_addr);
-            port = (ipv6->sin6_port);
-            ipver = "IPv6";
+
+    for(p = servinfo; p!=NULL; p=p->ai_next) {
+
+        if((sockfd=socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror(("socket::server"));
+            continue;
         }
-        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
-        printf("%s %s %d\n", ipver, ipstr, port);
+        // read
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == 1) {
+            perror("setsockopt");
+            return 2;
+        }
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == 1) {
+            close(sockfd);
+            perror("bind");
+            continue;
+        }
+        break;
     }
 
+    if (p==NULL) {
+        fprintf(stderr, "server failed to bind\n");
+        return 3;
+    }
+    freeaddrinfo(servinfo);
+    // ====================================================================================
+    // ************************************************************************************
 
-    freeaddrinfo(res);
+    if (listen(sockfd, BACKLOG)==-1) {
+        perror("listen");
+        return 4;
+    }
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if(sigaction(SIGCHLD, &sa, NULL)==-1) {
+        perror("sigaction");
+        return 5;
+    }
+    // ====================================================================================
+    // ************************************************************************************
+
+    printf("Server wait to connections...\n");
+
+    while (1) {
+        sin_size = sizeof(their_addr);
+        newfd = accept(sockfd, (struct sockaddr*)&their_addr, &sin_size);
+        if (newfd==-1){
+            perror("accept");
+            continue;
+        }
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr*)&their_addr), s, sizeof(s));
+        printf("Server got a connection from %s\n", s);
+
+
+        if (!fork()) {
+            close(sockfd);
+            if (send(newfd, "my first socket-server", 22, 0) == -1) {
+                perror("send");
+            }
+            close(newfd);
+            exit(0);
+        }
+        close(newfd);
+    }
     return 0;
 }
